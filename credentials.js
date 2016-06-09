d967f200fe9c002366fe5d445bf31bc044ea218b
Dean Hetherington <deanhet@gmail.com>
2016-06-09 19:44:17 +0100

    Emptied credentials

---
+++ credentials.js
@@ -0,0 +1,10 @@
+//Look for country codes here - https://cloud.google.com/translate/v2/translate-reference#supported_languages
+const credentials = {
+  translateAPIKey: '',
+  fbEmail: '',
+  fbPassword: '',
+  fromLanguage: '',
+  toLanguage: ''
+}
+
+export default credentials;
